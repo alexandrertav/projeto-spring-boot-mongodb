23ad47f577fc2954af94e5d8c3fc0940238c2e24
Alexandre Tavares <xandy.poa@hotmail.com>
2025-07-01 12:22:14 -0300

    Consultas simples

--- src/main/java/com/alextavares/workshopmongo/services/PostService.java
+++ src/main/java/com/alextavares/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.alextavares.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alextavares.workshopmongo.domain.Post;
-import com.alextavares.workshopmongo.domain.User;
 import com.alextavares.workshopmongo.repository.PostRepository;
 import com.alextavares.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {	
 		Optional<Post> obj = repo.findById(id);		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
